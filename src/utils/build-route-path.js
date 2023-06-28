6feeaef30ece0d67957413f2e218e5b6772d3c34
Thiago Jose Gontijo Cardoso <thiago.gontijo@mj.gov.br>
2023-06-28 18:19:33 -0300

    Update build-route-path.js

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,6 +1,6 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
-  const paramsWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+  const paramsWithParams = path.replace(new RegExp(routeParametersRegex, 'g'), '(?<$1>[a-z0-9\-_]+)');
 
   const pathRegex = new RegExp(`^${paramsWithParams}(?<query>\\?(.*))?$`)
 
